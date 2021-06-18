4b637ab73366be77da16acaf09e41883668dec2f
Ekram70 <ekramullah70@gmail.com>
2021-06-19 00:07:52 +0600

    tiny bug fixed in js

--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ let featureBoxIcon = document.querySelectorAll(".features-items-box-icon");
 for (let i = 0; i < featureBox.length; i++) {
   let imgNumber = i + 1;
   featureBox[i].addEventListener("mouseenter", function () {
-    featureBoxIcon[i].src = "../images/feature-box-" + imgNumber + "-white.png";
+    featureBoxIcon[i].src = "images/feature-box-" + imgNumber + "-white.png";
   });
   featureBox[i].addEventListener("mouseleave", function () {
-    featureBoxIcon[i].src = "../images/feature-box-" + imgNumber + "-grad.png";
+    featureBoxIcon[i].src = "images/feature-box-" + imgNumber + "-grad.png";
   });
 }
